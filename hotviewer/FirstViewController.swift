5d96e5d4c939d119b2021c592e3e2701f237e0da
傑瑞 <jerry@jieruideMac-mini.local>
2015-07-13 22:09:11 +0800

    Initial Commit

---
+++ hotviewer/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  hotviewer
+//
+//  Created by 傑瑞 on 2015/7/13.
+//  Copyright (c) 2015年 傑瑞. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ hotviewer/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  hotviewer
+//
+//  Created by 傑瑞 on 2015/7/13.
+//  Copyright (c) 2015年 傑瑞. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
