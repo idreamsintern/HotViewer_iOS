fad9f1eeb3449549cfa13fda45af8cc2c3bb0f61
Fonger <afong.room@gmail.com>
2015-07-18 07:53:17 +0800

    Hide tags block and prettify the hyper links for ContentParty article

--- hotviewer/WebViewController.swift
+++ hotviewer/WebViewController.swift
@@ -16,7 +16,9 @@ class WebViewController : UIViewController {
     var rawContent: String?
     
     override func viewWillAppear(animated: Bool) {
-        if let rawContent = self.rawContent {
+        if var rawContent = self.rawContent {
+            // Hide the tag list and prettify hyper link
+            rawContent += "<style>.lucy-detail p:nth-child(2){display:none;}img{width:100%}a{color:#337ab7;text-decoration:none}a:focus,a:hover{color:#23527c;text-decoration:underline}a:focus{outline:dotted thin;outline:-webkit-focus-ring-color auto 5px;outline-offset:-2px}</style>"
             webView.loadHTMLString(rawContent, baseURL: nil)
         }
         var shareBtn : UIBarButtonItem = UIBarButtonItem(title: "分享", style: UIBarButtonItemStyle.Plain, target: self, action: "shareClick:")
