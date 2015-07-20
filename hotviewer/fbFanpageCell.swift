3089ef046a1f50f67e6c7824dc01e88e3355a4a2
AndyChen1204 <andy821204@gmail.com>
2015-07-20 22:19:55 +0800

    add ptt and fbFanpage cell view

---
+++ hotviewer/fbFanpageCell.swift
@@ -0,0 +1,56 @@
+//
+//  fbFanpageCell.swift
+//  hotviewer
+//
+//  Created by AndyChen on 2015/7/20.
+//  Copyright (c) 2015年 傑瑞. All rights reserved.
+//
+
+import UIKit
+
+class fbFanpageCell: UITableViewCell {
+
+    @IBOutlet weak var thumbanilImageView: UIImageView!
+    @IBOutlet weak var content: UILabel!
+    @IBOutlet weak var fanCount: UILabel!
+    let loadingText = "載入中..."
+    
+    private var _thumbnailURL: NSURL!
+    var thumbnailURL: NSURL? {
+        get {
+            return self._thumbnailURL
+        }
+        set(url) {
+            if let url = url {
+                SimpleCache.sharedInstance.getImage(url) { image, error in
+                    if let err = error {
+                        println(err)
+                    } else if let fullImage = image {
+                        self.thumbanilImageView.image = fullImage
+                        self._thumbnailURL = url
+                    }
+                }
+            }
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    override func prepareForReuse() {
+        if let url = self.thumbnailURL {
+            SimpleCache.sharedInstance.cancelImage(thumbnailURL)
+        }
+        thumbanilImageView.image = nil
+        content.text = loadingText
+    }
+
+}
---
+++ hotviewer/pttCell.swift
@@ -0,0 +1,30 @@
+//
+//  pttCell.swift
+//  hotviewer
+//
+//  Created by AndyChen on 2015/7/20.
+//  Copyright (c) 2015年 傑瑞. All rights reserved.
+//
+
+import UIKit
+
+class pttCell: UITableViewCell {
+
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var board: UILabel!
+    @IBOutlet weak var push: UILabel!
+    @IBOutlet weak var thumbnailmageView: UIImageView!
+    
+   
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
